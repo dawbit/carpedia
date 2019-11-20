a5a8c24b7b546f710829cc64ec25b4d01505fbf2
Dawid Bitner <dbitner@wasko.pl>
2019-11-20 07:18:42 +0100

    Initial commit

---
+++ src/main/java/com/carpedia/carpedia/CarpediaApplication.java
@@ -0,0 +1,13 @@
+package com.carpedia.carpedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpediaApplication.class, args);
+	}
+
+}
