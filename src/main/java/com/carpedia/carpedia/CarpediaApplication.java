1042e68ba44597947e48ded45eef9cfb6b71cb7f
dawbit <dawbitner@gmail.com>
2019-11-21 07:23:07 +0100

    added hibernate, now automatically table is created (i belive)

--- src/main/java/com/carpedia/carpedia/CarpediaApplication.java
+++ src/main/java/com/carpedia/carpedia/CarpediaApplication.java
@@ -4,8 +4,10 @@ import com.carpedia.carpedia.repository.SimplyCarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class CarpediaApplication implements CommandLineRunner {
 
