622a06fb0f8c7d858c9072e0697be95441e079a7
dawbit <dawbitner@gmail.com>
2019-11-27 08:35:23 +0100

    Change settings. The database now retains its status after the program has finished.

--- src/main/java/com/carpedia/carpedia/CarpediaApplication.java
+++ src/main/java/com/carpedia/carpedia/CarpediaApplication.java
@@ -1,7 +1,5 @@
 package com.carpedia.carpedia;
 
-import com.carpedia.carpedia.repository.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -11,29 +9,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarpediaApplication implements CommandLineRunner {
 
-	@Autowired
-	SimplyCarRepository repository;
-
-	@Autowired
-	CountryRepository country;
-
-	@Autowired
-    SegmentRepository segment;
-
-	@Autowired
-	BodyTypeRepository bodytype;
-
-	@Autowired
-	EngineRepository engine;
-
-	@Autowired
-	CompanyRepository company;
-
-	@Autowired
-	UserRepository user;
-
-	@Autowired
-	CarRepository car;
+//	@Autowired
+//	SimplyCarRepository repository;
+//
+//	@Autowired
+//	CountryRepository country;
+//
+//	@Autowired
+//    SegmentRepository segment;
+//
+//	@Autowired
+//	BodyTypeRepository bodytype;
+//
+//	@Autowired
+//	EngineRepository engine;
+//
+//	@Autowired
+//	CompanyRepository company;
+//
+//	@Autowired
+//	UserRepository user;
+//
+//	@Autowired
+//	CarRepository car;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarpediaApplication.class, args);
@@ -42,14 +40,14 @@ public class CarpediaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... arg0) throws Exception {
 		// clear all record if existed
-		repository.deleteAll();
-		country.deleteAll();
-		segment.deleteAll();
-		bodytype.deleteAll();
-		engine.deleteAll();
-		company.deleteAll();
-		user.deleteAll();
-		car.deleteAll();
+//		repository.deleteAll();
+//		country.deleteAll();
+//		segment.deleteAll();
+//		bodytype.deleteAll();
+//		engine.deleteAll();
+//		company.deleteAll();
+//		user.deleteAll();
+//		car.deleteAll();
 	}
 
 }
